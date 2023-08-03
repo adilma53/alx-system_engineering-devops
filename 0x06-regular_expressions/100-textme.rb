b3d85739d706e61747319dff388071f3b09bfd4f
adil_ma <itsadilhadid@gmail.com>
2023-08-03 21:18:43 +0100

    regex is awesome

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
 
 # Regular expression pattern to extract the required information
-pattern = /(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/
-puts ARGV[0].scan(pattern).join(',')
+puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
