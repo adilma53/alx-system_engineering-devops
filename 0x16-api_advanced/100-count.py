25752d49bf7084110162f66e4d28f1d8d0f6493e
adil mae <itsadilhadid@gmail.com>
2023-11-09 21:11:17 +0100

    last task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python3
 """ parses titles of hot articles """
 
-import requests
-import sys
+from requests import get
+from sys import argv
+
+subreddit_titles = []
 after = None
-count = []
+
+
+def count_all(subreddit_titles, word_list):
+    """ parses hot articles """
+
+    count_dict = {word.lower(): 0 for word in word_list}
+    for title in subreddit_titles:
+        words = title.split(' ')
+        for word in words:
+            if count_dict.get(word) is not None:
+                count_dict[word] += 1
+
+    for key in sorted(count_dict, key=count_dict.get, reverse=True):
+        if count_dict.get(key):
+            for thing in word_list:
+                if key == thing.lower():
+                    print("{}: {}".format(thing, count_dict[key]))
 
 
 def count_words(subreddit, word_list):
-    """ parse hot articles """
+    global subreddit_titles
     global after
-    global count
-    headers = {'User-Agent': 'xica369'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    parameters = {'after': after}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+    """subs"""
+    headers = {'User-Agent': 'Dan Kazam'}
+    if after:
+        response = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+            subreddit, after), headers=headers).json().get('data')
+    else:
+        response = get('https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=headers).json().get('data')
+    subreddit_titles += [dic.get('data').get('title').lower()
+                         for dic in response.get('children')]
+    after = response.get('after')
+    if after:
+        return count_words(subreddit, word_list)
+    return count_all(subreddit_titles, word_list)
+
+
+if __name__ == "__main__":
+    count_words(argv[1], argv[2].split(' '))
