5512ccf2427e1e22e122ca7413c57c80645f9daa
adil mae <itsadilhadid@gmail.com>
2023-11-09 21:02:00 +0100

    last task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ import requests
 def count_words(subreddit, word_list, found_list=[], after=None):
     """get words of hot posts"""
     headers = {"User-agent": "Mozilla/5.0"}
-    posts = requests.get(f"http://www.reddit.com/r/{subreddit}/\
+    posts = requests.get(f"http://www.reddit.com/r/{subreddit}/
                             hot.json?after={after}", headers=headers)
     if after is None:
         word_list = [word.lower() for word in word_list]
